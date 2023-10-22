e8f8fcb5915c3ff040620372ddc7d771c2f22949
Edgardo Luis Jimenez <edgardotecno@gmail.com>
2023-10-21 22:15:50 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HomePage } from './HomePage';
+import { BlogPage } from './BlogPage';
+import { ProfilePage } from './ProfilePage';
+import { Menu } from './Menu';
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/blog' element={<BlogPage />}></Route>
+          <Route path='/profile' element={<ProfilePage />}></Route>
+          <Route path='*' element={<p>Not found</p>}></Route>
+        </Routes>
+      </HashRouter>
+    </>
+  )
+}
+
+export { App };
